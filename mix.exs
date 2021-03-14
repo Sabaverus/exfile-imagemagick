026986a1e6a3b1b22e8e0d9959c8f577403f4efa
Sergey <bobrano@yandex.ru>
2021-03-14 22:29:48 +0400

    Changed dependency

--- mix.exs
+++ mix.exs
@@ -32,7 +32,7 @@ defmodule ExfileImagemagick.Mixfile do
 
   defp deps do
     [
-      {:exfile, "~> 0.3.1"},
+      {:exfile, github: "desoulter/exfile", ref: "master"},
       {:poolboy, "~> 1.5.1"},
       {:earmark, "~> 0.1", only: :dev},
       {:ex_doc, "~> 0.11", only: :dev}
